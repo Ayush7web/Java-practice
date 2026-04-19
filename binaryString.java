fe8c6fe41e25487848abc8d0c6b8ae13ead31863
Ayush7web <ayushk93516@gmail.com>
2026-04-19 12:21:07 +0530

    binaryString

---
+++ binaryString.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class binaryString {
+
+  public static void findDuplicate(int n , int lastPlace, String str){
+// base case
+if(n == 0){
+
+  System.out.println(str);
+  return;
+}
+
+    //kaam
+
+    if(lastPlace == 0){
+
+      //sit 0 on chair n;
+      findDuplicate(n-1, 0, str+"0");
+      findDuplicate(n-1, 1, str+"1");
+    }else{
+      findDuplicate(n - 1, 0, str="0");
+    }
+  }
+  public static void main(String[] args) {
+    findDuplicate(3,0, "");
+  }
+}  
