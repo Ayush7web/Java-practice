b9d412169b00c72f0e6006c5bd0b2674cb7ffbee
Ayush7web <ayushk93516@gmail.com>
2026-05-04 12:45:04 +0530

    sl

--- LinkedList/Singly.java
+++ LinkedList/Singly.java
@@ -9,21 +9,33 @@ public class Singly {
       this.data = data;
       this.next = null;
     }
-    public Node1(int data ){
-      this.data = data;
-      this.next = null;
-      return;
-    }
-    
   }
 
   public static Node head;
   public static Node tail;
+
+  public void addFirst(int data){
+    // step 1 create a new Node
+    Node newNode = new Node(data);
+if(head == null){
+  head = tail = newNode;
+  return;
+}
+    // step 2 - newnode next = head;
+    newNode.next = head; // link
+
+    // step 3- head = newnode
+    head = newNode;
+  }
   public static void main(String[] args) {
     Singly  sl   =   new  Singly();
-    sl.head = new Node(1);
-    sl.head.next  =  new Node(2);
-  System.out.println(sl);  
+    // sl.head = new Node(1);
+    // sl.head.next  =  new Node(2);
+  // System.out.println(sl);  
+  sl.addFirst(1);
+  sl.addFirst(2);
+  System.out.println();  
   }
+  
     
 }
