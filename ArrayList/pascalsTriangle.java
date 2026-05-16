dd73f53d31cf5d9e489fca09315ffdd8a8752f7e
Ayush7web <ayushk93516@gmail.com>
2026-05-16 17:34:19 +0530

    printPascal

---
+++ ArrayList/pascalsTriangle.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class pascalsTriangle {
+
+  public static void printPascal(int n){
+    // int i,j;
+    for(int i=0; i<n; i++){
+      for(int j=0; j<n; j++){
+        
+      }
+    }
+  }
+  public static void main(String[] args) {
+    int ans[][] = new int[n][n];
+  }
+}
