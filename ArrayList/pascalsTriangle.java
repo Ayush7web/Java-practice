1a0bf5b665bf10939b696b6cda31743364cf7650
Ayush7web <ayushk93516@gmail.com>
2026-05-19 17:04:21 +0530

    pascalsTri

--- ArrayList/pascalsTriangle.java
+++ ArrayList/pascalsTriangle.java
@@ -4,13 +4,40 @@ public class pascalsTriangle {
 
   public static void printPascal(int n){
     // int i,j;
-    for(int i=0; i<n; i++){
-      for(int j=0; j<n; j++){
-        
+    for(int i=0; i<=n; i++){
+      for(int j=0; j<i; j++){
+        if (j == i) {
+          j++;
+        }
+        System.out.println(i);
       }
+      // System.out.println("the pascalsTriangle + " );
     }
+    
   }
   public static void main(String[] args) {
-    int ans[][] = new int[n][n];
+    int ans[] = new int[6];
+    // System.out.println(printPascal(3));
+    printPascal(6);
   }
 }
+
+
+
+
+
+
+
+
+//                   1
+//                 1 2 1
+//                1 2 3 1
+//
+//
+//
+//
+//
+//
+//
+//
+//
