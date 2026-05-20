a343e9ee225500f141091ae8404b1b079a382c60
Ayush7web <ayushk93516@gmail.com>
2026-05-20 12:52:29 +0530

    solve

--- ArrayList/pascalsTriangle.java
+++ ArrayList/pascalsTriangle.java
@@ -2,23 +2,29 @@ import java.util.*;
 
 public class pascalsTriangle {
 
-  public static void printPascal(int n){
+  public static void printPascal(int n, int[][] ans){
     // int i,j;
     for(int i=0; i<=n; i++){
       for(int j=0; j<i; j++){
-        if (j == i) {
-          j++;
+        if (j == 0 || j == i) {
+          ans[i][j] = 1;
+
+          // System.out.println("print the term  " + j);
+
+        }else{
+          ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
         }
-        System.out.println(i);
+        System.out.println("the print " + ans[i][j]);
       }
-      // System.out.println("the pascalsTriangle + " );
+     
     }
     
   }
   public static void main(String[] args) {
-    int ans[] = new int[6];
+    int n = 4;
+    int ans[][] = new int[n+1][n+1];
     // System.out.println(printPascal(3));
-    printPascal(6);
+    printPascal(4, ans);
   }
 }
 
