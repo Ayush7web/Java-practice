731496399615a135eeb6e231d8d6d7ca87681160
Ayush7web <ayushk93516@gmail.com>
2026-05-25 18:58:26 +0530

    solve

--- LinkedList/Intersection.java
+++ LinkedList/Intersection.java
@@ -1,22 +1,49 @@
 
 import java.util.*;
+
 public class Intersection {
-  public static void meetPoint(int num1[], int num2[]){
-   for(int i=0; i <= num1.length-1; i++){
-    for(int j = 0; j <= num2.length-1; j++){
-      if (num1[i] == num2[j]) {
-        int temp = num2[j];
-        num2[j] = num1[i];
-        num1[i] = temp;
-        // System.out.println("threb");
-        return;
+  static class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+      this.data = data;
+    }
+  }
+
+  public static void meetPoint(Node num1, Node num2) {
+
+    if (num1 == null || num2 == null) {
+      return;
+    }
+
+    Node p1 = num1, p2 = num2;
+
+    while (num1 != num2) {
+      if (num1 == null) {
+        num1 = p2;
+      } else if (num2 == null) {
+        num2 = p1;
+      } else {
+        num1 = num1.next;
+        num2 = num2.next;
       }
     }
-   }
+    System.out.println(num1.data);
   }
+
   public static void main(String[] args) {
-    int num1[] = {1,4,7,3};
-    int num2[] = {2,5,9,3};
-    meetPoint(num1, num2);
+    Node newNode = new Node(1);
+
+    Node num1 = new Node(1);
+    num1.next = new Node(4);
+    num1.next.next = new Node(7);
+    num1.next.next.next = new Node(3);
+
+    Node num2 = new Node(2);
+    num2.next = new Node(5);
+    num2.next.next = new Node(9);
+    num2.next.next.next = new Node(3);
+
   }
 }
