f752f3719e04bcfe00bd602d06ed9fe72f9be93a
Ayush7web <ayushk93516@gmail.com>
2026-09-02 22:05:51 +0530

    func_over

--- ApnaCollege/Func_overloading.java
+++ ApnaCollege/Func_overloading.java
@@ -12,8 +12,15 @@ public class Func_overloading {
     System.out.println("Multiply two numbers");
     return result;
   }
+
+  public static float twoSum(float a , float b){
+    float result = a + b;
+    System.out.println("Adding two numbers");
+    return result;
+  }
   public static void main(String[] args) {
     System.out.println(twoNoMult(3, 5, 6));
     System.out.println(twoNoMult(3, 5));
+    System.out.println(twoSum(4, 8));
   }
 }
