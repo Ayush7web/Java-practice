f6839b894a445c1262e72992b55b3706d5774bae
Ayush7web <ayushk93516@gmail.com>
2026-09-02 21:56:40 +0530

    func_over

---
+++ ApnaCollege/Func_overloading.java
@@ -0,0 +1,19 @@
+package ApnaCollege;
+
+public class Func_overloading {
+  public static int twoNoMult(int a , int b){
+     int result = a * b;
+    System.out.println("Multiply two numbers");
+    return result;
+  }
+
+  public static int twoNoMult(int a , int b , int c){
+    int result = a * b* c;
+    System.out.println("Multiply two numbers");
+    return result;
+  }
+  public static void main(String[] args) {
+    System.out.println(twoNoMult(3, 5, 6));
+    System.out.println(twoNoMult(3, 5));
+  }
+}
