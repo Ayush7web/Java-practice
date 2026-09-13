05c75847a2cb5cdeecec7fd22e0963391a6d5f7b
Ayush7web <ayushk93516@gmail.com>
2026-09-13 18:40:33 +0530

    sorted recursion

--- ApnaCollege/Fibonacci.java
+++ ApnaCollege/Fibonacci.java
@@ -17,17 +17,32 @@ public class Fibonacci {
     return fn;
   }
 
+  public static boolean isSorted(int arr[], int i) {
+    if (i == arr.length - 1) {
+      return true;
+    }
+
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
+
+    return isSorted(arr, i + 1);
+  }
+
   public static void main(String[] args) {
-    int n = 26;
-    System.out.println(fib(21));
-    System.out.println(fib(22));
-    System.out.println(fib(23));
-    System.out.println(fib(24));
-    System.out.println(fib(25));
-    System.out.println(fib(26));
+    // int n = 26;
+    // System.out.println(fib(21));
+    // System.out.println(fib(22));
+    // System.out.println(fib(23));
+    // System.out.println(fib(24));
+    // System.out.println(fib(25));
+    // System.out.println(fib(26));
+
+    int arr[] = {23,4,3,5,6,43,65,21};
+    // int arr[] = { 1, 2, 3, 4, 5 };
+    System.out.println(isSorted(arr, 0));
 
   }
 }
-
 
 // i have to solve exactly questions in Dynamic programming
