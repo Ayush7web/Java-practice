3baa45b4b0243852a8471bf43117b13243f7c8b4
Ayush7web <ayushk93516@gmail.com>
2026-09-17 11:42:44 +0530

    last occurence

--- ApnaCollege/FindOccur.java
+++ ApnaCollege/FindOccur.java
@@ -1,20 +1,32 @@
 package ApnaCollege;
 
-public class FindOccur{
-  public static int findIndex(int arr[], int key, int i) {
-    if (i == arr.length) {
+public class FindOccur {
+  // public static int findIndex(int arr[], int key, int i) {
+  // if (i == arr.length) {
+  // return -1;
+  // }
+
+  // if (arr[i] == key) {
+  // return i;
+  // }
+
+  // return findIndex(arr, key, i+1);
+  // }
+
+  public static int lastIndex(int arr[], int key, int i) {
+    if(i == arr.length){
       return -1;
     }
-
-    if (arr[i] == key) {
+    int isFound = lastIndex(arr, key, i + 1);
+    if (isFound == -1 && arr[i] == key) {
       return i;
     }
+    return isFound;
 
-    return findIndex(arr, key, i+1);
   }
 
   public static void main(String args[]) {
-int arr[] = {3,4,3,5,5,5,32,32212,2,54};
-System.out.println(findIndex(arr, 322122, 0));
+    int arr[] = { 3, 4, 32, 3, 5, 5, 5, 32, 32212, 2, 54 };
+System.out.println(lastIndex(arr, 32, 0));
   }
 }
